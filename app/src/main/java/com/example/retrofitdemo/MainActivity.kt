446d0090adbae0466968bac2d0225d5aee1a223c
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2023-06-14 03:48:53 +0500

    Showing fetched data in recycler view done

--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -3,27 +3,52 @@ package com.example.retrofitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofitdemo.adapter.UsersRvAdapter
 import com.example.retrofitdemo.apiinterface.UsersApiInterface
 import com.example.retrofitdemo.apiinterface.UsersRetrofitUtilities
+import com.example.retrofitdemo.databinding.ActivityMainBinding
+import com.example.retrofitdemo.model.UsersModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var usersApi: UsersApiInterface
+    private lateinit var usersRvAdapter: UsersRvAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //! recycler view
+        binding.usersRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        //! setting retrofit object.
         usersApi = UsersRetrofitUtilities.getRetrofitInstance().create(UsersApiInterface::class.java)
+
 
         lifecycleScope.launch {
             val result = usersApi.getUsers()
 
             if (result.body() != null) {
-                Log.d("data", "${result.body()!![0]}")
+                showAllUsers(result.body())
             }
+        }
+    }
+
+    private fun showAllUsers(list: List<UsersModel>?) {
+        if (list?.isNotEmpty()!!) {
+            binding.usersRecyclerView.visibility = View.VISIBLE
+            binding.tvNoRecords.visibility = View.GONE
+            usersRvAdapter = UsersRvAdapter(list)
+            binding.usersRecyclerView.adapter = usersRvAdapter
+        } else {
+            binding.usersRecyclerView.visibility = View.GONE
+            binding.tvNoRecords.visibility = View.VISIBLE
         }
     }
 }
